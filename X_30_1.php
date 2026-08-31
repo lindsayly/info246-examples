febd8ef4b653e6d201899a46b0a31856f5893323
lindsayly <101374847+lindsayly@users.noreply.github.com>
2026-08-31 12:11:46 -0500

    Create X_30_1.php

---
+++ X_30_1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>String Delimiters</title>
+  </head>
+  <body>
+    <?php
+      print("I'm happy you showed up Mr. O'Reilly,");
+      print("Andre said, \"I don't like to wait!\"");
+  </body>
+</html>
