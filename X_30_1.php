b4ab508cd30941c90c5c176cb1c1d6d92f99c9ae
lindsayly <101374847+lindsayly@users.noreply.github.com>
2026-08-31 12:15:24 -0500

    Update X_30_1.php

--- X_30_1.php
+++ X_30_1.php
@@ -5,7 +5,7 @@
   </head>
   <body>
     <?php
-      print("I'm happy you showed up Mr. O'Reilly,");
+      print("\"I'm happy you showed up Mr. O'Reilly,\"\n");
       print("Andre said, \"I don't like to wait!\"");
     ?>
   </body>
