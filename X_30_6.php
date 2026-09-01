9f830c114c39b5ee9ea88f97a335424291aff2f2
lindsayly <101374847+lindsayly@users.noreply.github.com>
2026-09-01 17:47:39 -0500

    Create X_30_6.php

---
+++ X_30_6.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Nested IF</title>
+  </head>
+  <body>
+    <h3>Who are you?</h3>
+    <?php
+      $fName = $_POST['firstName'];
+      $color = $_POST['eyeColor'];
+      if($fname == 'Steve'){
+        print("<p>You are the teacher</p>");
+        if($color == 'blue'){
+          print("<p>. \nHi, blue eyes!</p>");
+        } else {
+          print("<p>, \nbut I thought you had blue eyes.</p>");
+        }
+      } else {
+        print("<p>You are $fName</p>");
+        if($color == 'blue'){
+          print("<p>. \nHi, blue eyes!</p>");
+        } else {
+          print("<p>and your eye color is $color</p>");
+        }
+      }
+    ?>
+  </body>
+</html>
