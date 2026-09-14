4e0b3af1196fb3eb1a07889bfadc87509b591f33
lindsayly <101374847+lindsayly@users.noreply.github.com>
2026-09-14 16:26:32 -0500

    Update X_40_3.php

--- module-40/X_40_3.php
+++ module-40/X_40_3.php
@@ -7,8 +7,7 @@
     <h3>Team Members</h3>
     <ol>
       <?php
-        $DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
-        $filename = $DOCUMENT_ROOT.'X_40_3.txt';
+        $filename = 'X_40_3.txt';
         $numLines = count(file($filename));
         $fp = fopen($filename, 'r');
         for ($i = 1; $i <= $numLines; $i++)
